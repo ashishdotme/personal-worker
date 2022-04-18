df9cf97ddbebde66ff746bb313ec5135d227185b
Ashish Patel <ashishsushilpatel@gmail.com>
2022-04-18 21:38:55 +0100

    build: change port

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(
-    configService.get<number>('app.port'),
-    configService.get<string>('app.host'),
-  );
+  await app.listen(8002);
 }
 bootstrap();
